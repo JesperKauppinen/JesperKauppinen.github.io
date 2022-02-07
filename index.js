dab8720f4089c048fdb88102893f28f1c4c45df4
JesperKauppinen <jesperkauppinen@gmail.com>
2022-02-07 19:24:52 +0200

    commented index.js

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 
 // toggle button display between block and none
+// and toggle folding symbol
 function toggle(parent) {
 
-    // Get sibling of parent element
+    // Get list element
     const sibling = parent.nextElementSibling;
+    // Get →/↓ symbol element
     const child = parent.children;
 
-    console.log(sibling.style.display);00
-
+    // If folding element is closed ( →)
+    // display list element and change folding symbol to open
     if (child[0].innerHTML == " →") {
         sibling.style.display = "block";
         child[0].innerHTML = " ↓";
     }
+    // If folding symbol is not closed
+    // close symbol and hide list
     else {
         sibling.style.display = "none";
         child[0].innerHTML = " →";
